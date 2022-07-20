2ce9f3de0c283acc038192d433347422ac3deb6c
Fumbani Banda <60394916+deadex-ng@users.noreply.github.com>
2022-07-20 23:50:49 +0200

    Add files via upload

---
+++ englishSA.py
@@ -0,0 +1,60 @@
+#list of negative English words
+negative = ['bad', 'trash','dislike']
+
+#list of positive English words 
+positive = ['good','nice','like']
+
+#list of stop words 
+stopwords =['is','that','this','the']
+
+
+def get_polarity(text):
+
+    #counter for postive polarity
+    counter_pos = 0
+
+    #counter for negative polarity
+    counter_neg = 0
+
+    #split text to list
+    txt_list = text.split()
+
+    #remove stopwords for text 
+    for word in list(txt_list):
+        if word in stopwords:
+            txt_list.remove(word)
+
+    #count number of positive or negative word in text
+    for word in txt_list:
+        if word in positive:
+            counter_pos = counter_pos + 1
+        if word in negative:
+            counter_neg = counter_neg + 1
+
+    if counter_pos > counter_neg:
+        return 'Positive'
+    elif counter_pos < counter_neg:
+        return 'Negative'
+    else:
+        return 'Neutral'
+
+#Here are sample statements we are using to test
+text_one = 'this car is nice'
+print('-----------text and polarity------------')
+print('Text: ', text_one)
+print('Polarity:', get_polarity(text_one))
+print('----------------------------------------')
+
+
+text_two = 'that is bad'
+print('-----------text and polarity------------')
+print('Text: ', text_two)
+print('Polarity:', get_polarity(text_two))
+print('----------------------------------------')
+
+
+text_three = 'the design is good but the color is bad'
+print('-----------text and polarity------------')
+print('Text: ', text_three)
+print('Polarity:', get_polarity(text_three))
+print('----------------------------------------')
